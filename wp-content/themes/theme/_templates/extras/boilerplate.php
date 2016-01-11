9d2df1182b854e6f13f28b0364be671acbd53eb4
Max Lutz <maxlutzfl@gmail.com>
2016-01-11 17:03:56 -0500

    wadawd

---
+++ wp-content/themes/theme/_templates/extras/boilerplate.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html prefix="og: http://ogp.me/ns#">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>	
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+	<div id="SiteMainWrapper">
+
+		<nav id="SiteNavigation" role="banner"></nav>
+		<main id="SiteMain" role="main"></main>
+		<footer id="SiteFooter" role="contentinfo"></footer>
+
+	</div>
+
+	<div id="MobileNavigation">
+	</div>
+	
+	<?php wp_footer(); ?>
+</body>
+</html>
