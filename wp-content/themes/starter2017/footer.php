b4c64dab112a98875ec9d3eaa6718043df9ca8d0
Max Lutz <maxlutzfl@gmail.com>
2016-09-30 15:48:38 -0400

    move templates and front-end css/js to child theme

--- wp-content/themes/starter2017/footer.php
+++
@@ -1,4 +0,0 @@
-<?php get_template_part('templates/parts/footer'); ?>
-</div><!-- #site-wrapper -->
-<?php get_template_part('templates/parts/mobile-menu'); ?>
-<?php get_template_part('templates/document/footer'); ?>
