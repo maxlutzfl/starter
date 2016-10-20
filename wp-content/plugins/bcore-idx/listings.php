e0f3d5e718c18f8c8416ae7a44aeeef0cf07d49a
Max Lutz <maxlutzfl@gmail.com>
2016-10-20 11:36:49 -0400

    idx

--- wp-content/plugins/bcore-idx/listings.php
+++ wp-content/plugins/bcore-idx/listings.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
 <h1>Listing Details Page Template</h1>
+<?php global $wp_query; ?>
+
+<?php if ( array_key_exists('listingdetails', $wp_query->query_vars) ) : ?>
+	<?php wolfnet_get_property_details($wp_query->query_vars['listingdetails']); ?>
+<?php else : ?>
+	<?php wolfnet_api_testing(); ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
