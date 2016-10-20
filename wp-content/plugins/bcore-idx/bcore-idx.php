4390235be8465189d08d11ac058d893c4042b161
Max Lutz <maxlutzfl@gmail.com>
2016-10-20 11:11:50 -0400

    start

--- wp-content/plugins/bcore-idx/bcore-idx.php
+++ wp-content/plugins/bcore-idx/bcore-idx.php
@@ -7,8 +7,8 @@ Author: BrandCo LLC
 Author URI: https://brandco.com/
 */
 
-add_action('wp_footer', 'bcore');
-function bcore() {
+add_action('wp_footer', 'bcore_idx');
+function bcore_idx() {
 
 	$data = array(
 		"key" => "wp_1aec9f8eb41cfaabd582c7323789c92b", 
@@ -31,3 +31,91 @@ function bcore() {
 	echo '----';
 	var_dump($result);
 }
+// ---------------------------------------------------------------------------
+// Add virtual pages.
+// ---------------------------------------------------------------------------
+ 
+/**
+ * First create a query variable addition for the pages. This means that
+ * WordPress will recognize index.php?bcoreidx=name
+ */
+function example_bcoreidx_query_vars($vars) {
+  $vars[] = 'bcoreidx';
+  return $vars;
+}
+add_filter('query_vars', 'example_bcoreidx_query_vars');
+ 
+/**
+ * Add redirects to point desired virtual page paths to the new 
+ * index.php?bcoreidx=name destination.
+ *
+ * After this code is updated, the permalink settings in the administration
+ * interface must be saved before they will take effect. This can be done 
+ * programmatically as well, using flush_rewrite_rules() triggered on theme
+ * or plugin install, update, or removal.
+ */
+function example_bcoreidx_add_rewrite_rules() {
+  add_rewrite_tag('%bcoreidx%', '([^&]+)');
+  add_rewrite_rule(
+    'listings/?$',
+    'index.php?bcoreidx=listings',
+    'top'
+  );
+ 
+  // An alternative approach.
+  // add_rewrite_rule(
+  //   'vp/([^/]*)/?$',
+  //   'index.php?bcoreidx=$matches[1]',
+  //   'top'
+  // );
+ 
+  // There is also nothing stopping you from declaring more new variables
+  // via query_vars and mixing them in if a page needs additional parameters.
+  add_rewrite_tag('%listingdetails%', '([^&]+)');
+  add_rewrite_rule(
+    'listings/([^/]*)/?$',
+    'index.php?bcoreidx=listings&listingdetails=$matches[1]',
+    'top'
+  );
+}
+add_action('init', 'example_bcoreidx_add_rewrite_rules');
+ 
+/**
+ * Assign templates to the virtual pages.
+ */
+function example_bcoreidx_template_include($template) {
+  global $wp_query;
+  $new_template = '';
+ 
+  if ( array_key_exists('listingdetails', $wp_query->query_vars) ) {
+  	var_dump($wp_query->query_vars);
+  }
+
+  if (array_key_exists('bcoreidx', $wp_query->query_vars)) {
+    switch ($wp_query->query_vars['bcoreidx']) {
+
+      case 'listings':
+        // We expect to find bcoreidx-listings.php in the 
+        // currently active theme.
+        $new_template = dirname( __FILE__ ) . '/listings.php';
+        break;
+    }
+ 
+    if ($new_template != '') {
+      return $new_template;
+    } else {
+      // This is not a valid bcoreidx value, so set the header and template
+      // for a 404 page.
+      $wp_query->set_404();
+      status_header(404);
+      return get_404_template();
+    }
+  }
+ 
+  return $template;
+}
+add_filter('template_include', 'example_bcoreidx_template_include');
+
+
+
+
---
+++ wp-content/plugins/bcore-idx/listings.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<h1>Listing Details Page Template</h1>
+<?php get_footer(); ?>
