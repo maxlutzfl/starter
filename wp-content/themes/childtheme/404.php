cf96b106feb32e949d7a2bbbdb0e829e01ce827c
Max Lutz <maxlutzfl@gmail.com>
2016-12-06 10:35:57 -0500

    add 404 template

---
+++ wp-content/themes/childtheme/404.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * 404 Template
+ */
+get_header(); ?>
+<main id="site-main" class="site-main">
+	<section id="default-section" class="default-section">
+		<div class="section-container">
+			<h1>404</h1>
+			<div class="entry-content">
+				<p>This page does not exist.</p>
+			</div>
+		</div>
+	</section>
+</main>
+<?php get_footer(); ?>
